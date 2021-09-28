93ced07f9e1b1c2416169a9b7b613df0e14c9272
javond79 <89801226+javond79@users.noreply.github.com>
2021-09-28 11:53:23 -0500

    Add files via upload

---
+++ MyWorld.java
@@ -0,0 +1,20 @@
+
+package my.world;
+
+
+public class MyWorld {
+// My hello world - Javon Doss 2021 sep
+    public static void main(String[] args) {
+      
+      System.out.println("hello world")
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
